cfd3d3da735201c92b3f3ac23e207734722b9f9c
Emma Goodliffe <emma@piemag.com>
2020-11-22 16:30:52 +0000

    renamed build to dist

--- rollup.config.js
+++ rollup.config.js
@@ -39,7 +39,7 @@ export default {
     sourcemap: true,
     format: "iife",
     name: "app",
-    file: "public/build/bundle.js",
+    file: "public/dist/bundle.js",
   },
   plugins: [
     svelte({
